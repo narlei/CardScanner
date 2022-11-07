3b5d7e1f5e0270f54b0e08654911c9fffe1a5b05
Yoel Jimenez del Valle <yoel07@gmail.com>
2022-11-07 14:34:59 -0500

    SWPM

---
+++ Tests/CardScannerTests/CardScannerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CardScanner
+
+final class CardScannerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(CardScanner().text, "Hello, World!")
+    }
+}
